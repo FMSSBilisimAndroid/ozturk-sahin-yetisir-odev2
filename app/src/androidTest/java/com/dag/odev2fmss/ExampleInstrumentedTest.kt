29443aff916bb32d258d3549482000d2e1dc0c11
ozturksahinyetisir <108059951+ozturksahinyetisir@users.noreply.github.com>
2022-09-03 12:02:05 +0300

    package name changed

--- app/src/androidTest/java/com/dag/odev2fmss/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/dag/odev2fmss/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dag.odev2fmss", appContext.packageName)
+        assertEquals("com.ozturksahinyetisir.week2", appContext.packageName)
     }
 }
